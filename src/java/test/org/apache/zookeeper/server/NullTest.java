496a23d5ef4b174397d256689813123ab31320fb
Akshay Joshi <akshayaj@usc.edu>
2017-10-31 16:19:26 -0700

    Add test to check for null

---
+++ src/java/test/org/apache/zookeeper/server/NullTest.java
@@ -0,0 +1,27 @@
+package org.apache.zookeeper.server;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Set;
+
+import org.junit.Test;
+
+public class NullTest {
+
+    @Test
+    public void testGetChildren() {
+        DataNode dataNode = new DataNode();
+        Set<String> children = dataNode.getChildren();
+        assertNotNull(children);
+        assertEquals(0, children.size());
+
+        String child = "child";
+        dataNode.addChild(child);
+        dataNode.removeChild(child);
+        children = dataNode.getChildren();
+        assertNotNull(children);
+        assertEquals(0, children.size());
+    }
+
+}
